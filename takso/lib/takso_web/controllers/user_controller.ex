21a36e54e4867815e3355d3428f51b9a4df36a5a
David Chapela <chapelad@ut.ee>
2025-10-08 20:25:37 +0300

    Part 3: Providing feedback

--- takso/lib/takso_web/controllers/user_controller.ex
+++ takso/lib/takso_web/controllers/user_controller.ex
@@ -16,9 +16,14 @@ defmodule TaksoWeb.UserController do
 
   def create(conn, %{"user" => user_params}) do
     changeset = User.changeset(%User{}, user_params)
-
-    Repo.insert(changeset)
-    redirect(conn, to: ~p"/users")
+    case Repo.insert(changeset) do
+      {:ok, _user} ->
+        conn
+          |> put_flash(:info, "User created successfully.")
+          |> redirect(to: ~p"/users")
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 
   def edit(conn, %{"id" => id}) do
